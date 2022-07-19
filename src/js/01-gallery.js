3e3eb6ff02c46b1f7f1b48409ae96a0c0d74195a
chekirko <victorche59@gmail.com>
2022-07-19 12:35:05 +0300

    додає перше завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+function createGalleryItemsMarkup(arr) {
+  const markup = arr
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a>`;
+    })
+    .join(' ');
+  return markup;
+}
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.innerHTML = galleryItemsMarkup;
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
