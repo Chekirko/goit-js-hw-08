40c84fd6c0a0f8756f610cf2d780ce083aecccd6
chekirko <victorche59@gmail.com>
2022-07-19 14:12:44 +0300

    додає друге завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+function onTimeupdate(currentTime) {
+  const value = JSON.stringify(currentTime.seconds);
+  localStorage.setItem('videoplayer-current-time', `${value}`);
+}
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
