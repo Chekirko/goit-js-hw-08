2100d1b240d81b753062bf0a3ef7af723f3e536d
chekirko <victorche59@gmail.com>
2022-07-19 17:28:30 +0300

    додає третю домашку (без одного нюансу)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,38 @@
+import throttle from 'lodash.throttle';
+
 const refs = {
-  form: document.querySelector('feedback-form'),
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
 };
 
-function onFormInput(evt) {}
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
 
-refs.form.addEventListener('input', onFormInput);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(evt) {
+  //   formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.target.reset();
+  const parsedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log(parsedData);
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateForm() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    refs.input.value = parsedData[refs.input.name];
+    refs.textarea.value = parsedData[refs.textarea.name];
+  }
+}
+
+populateForm();
